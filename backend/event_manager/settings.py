58f6c0870e10136223f6bf79f91239494e522477
Nitesh Kumar <niteshkumar860326@gmail.com>
2026-09-25 23:22:27 +0530

    allow vercel frontend origin

--- backend/event_manager/settings.py
+++ backend/event_manager/settings.py
@@ -105,6 +105,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5501",
     "http://localhost:5501",
+    "https://frontend-dusky-tau-66.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -118,4 +119,5 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5501",
     "http://localhost:5501",
+    "https://frontend-dusky-tau-66.vercel.app",
 ]
